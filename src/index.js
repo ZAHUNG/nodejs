65a4da18c1e145a817d14b43a62fc462411bcdda
ZAHUNG <hunggia036@gmail.com>
2026-05-21 16:10:06 +0700

    Refactor code: create routes, app/controller folders

--- src/index.js
+++ src/index.js
@@ -3,17 +3,21 @@ const express = require("express");
 const handlebars = require("express-handlebars");
 const app = express();
 
+const route = require("./routes");
+//routes init
+route(app);
+
 app.use(express.static(path.join(__dirname,'public')));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.engine("hbs", handlebars.engine({extname: '.hbs'}));
 app.set("view engine", "hbs");
 app.set('views', path.join(__dirname, 'resources\\views'));
-app.get("/", (req, res) => {
-    res.render("home");
-});
-app.get("/news", (req, res) => {
-    res.render("news");
-});
+
+
+
 app.listen(3000, () => {
     console.log("Server chạy tại http://localhost:3000");
 });
